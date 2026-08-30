7b643b4537ebf9cf9ac7679fb6604409a9fa8179
Bhautik Kalsariya <bhautikkalsariya29@gmail.com>
2026-08-30 21:38:05 +0530

    Time: 1034 ms (8.29%), Space: 293.9 MB (5.99%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -16,8 +16,8 @@ public:
                     ans.insert(temp);
                     j++;
                     k--;
-                    while(j<k && arr[j]==arr[j-1])j++;
-                    while(j<k && arr[k]==arr[k+1])k--;
+                    //while(j<k && arr[j]==arr[j-1])j++;
+                    //while(j<k && arr[k]==arr[k+1])k--;
                 }
             }
         }
