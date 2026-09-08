fcefc975d54b38f40adfcdf636570246586cc4e5
Bhautik Kalsariya <bhautikkalsariya29@gmail.com>
2026-09-08 13:45:00 +0530

    Time: 0 ms (100%), Space: 30.1 MB (91.44%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
-bool even(vector<int> arr){
-    for(int i=0;i<arr.size();i++){
-        if(arr[i]%2!=0)return false;
-    }
-    return true;
-}
-bool odd(vector<int> arr){
-    for(int i=0;i<arr.size();i++){
-        if(arr[i]%2==0)return false;
-    }
-    return true;
-}
-    bool uniformArray(vector<int>& arr) {
-        if(even(arr))return true;
-        if(odd(arr))return true;
+    bool uniformArray(vector<int>& nums1) {
         return true;
     }
 };
